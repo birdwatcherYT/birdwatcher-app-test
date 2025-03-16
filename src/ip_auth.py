b6a810ddc6bc6840f2587003e440c88cbb0c46b5
tomoki-yoshida <tomoki.yoshida@dena.com>
2025-03-16 20:46:09 +0900

    bugfix

--- src/ip_auth.py
+++ src/ip_auth.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
-import requests
 from dotenv import load_dotenv
+from streamlit_javascript import st_javascript
 
 # ローカル用
 load_dotenv()
@@ -9,15 +9,20 @@ load_dotenv()
 IP_ADDRESSES = os.environ.get("IP_ADDRESSES", "").split(",")
 
 def get_client_ip_by_api():
-    url = 'https://api.ipify.org?format=json'
-    try:
-        response = requests.get(url)
-        result = response.json()
-        if 'ip' in result:
-            return result['ip']
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    return None
+     url = 'https://api.ipify.org?format=json'
+     script = (
+         f'await fetch("{url}").then('
+         'function(response) {'
+         'return response.json();'
+         '})'
+     )
+     try:
+         result = st_javascript(script)
+         if isinstance(result, dict) and 'ip' in result:
+             return result['ip']
+     except:
+        return ""
+     return None
 
 def get_client_ip_by_header():
     # ローカルでは動作確認できないので使っていない
@@ -36,12 +41,15 @@ def login():
     st.title("IP認証")
     ip_address = get_client_ip_by_api()
     # ip_address = get_client_ip_by_header()
-    if ip_address in IP_ADDRESSES:
+    if ip_address is None:
+        st.write("IPアドレス確認中...")
+    elif ip_address in IP_ADDRESSES:
         st.write(f"クライアントIPアドレス: {ip_address}")
         st.success("ログイン成功！")
         st.session_state.authenticated = True
         st.rerun()
     else:
+        st.write(f"クライアントIPアドレス: {ip_address}")
         st.error("許可されていないIPアドレスです")
         st.session_state.authenticated = False
 
